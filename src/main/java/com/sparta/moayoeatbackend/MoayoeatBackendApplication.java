410cb5db665742bbe537b3aa3b84705b5fa0af09
minahYu <mina010311@naver.com>
2024-01-05 10:28:06 +0900

    init

---
+++ src/main/java/com/sparta/moayoeatbackend/MoayoeatBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.moayoeatbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoayoeatBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoayoeatBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/moayoeatbackend/MoayoeatBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.moayoeatbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoayoeatBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
