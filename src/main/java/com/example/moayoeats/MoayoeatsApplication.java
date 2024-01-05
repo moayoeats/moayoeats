4a98b3cb921751efc8120a83fdd295d02b2fe025
minahYu <mina010311@naver.com>
2024-01-05 20:53:58 +0900

    init commit

---
+++ src/main/java/com/example/moayoeats/MoayoeatsApplication.java
@@ -0,0 +1,13 @@
+package com.example.moayoeats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoayoeatsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoayoeatsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/moayoeats/MoayoeatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.moayoeats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoayoeatsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
