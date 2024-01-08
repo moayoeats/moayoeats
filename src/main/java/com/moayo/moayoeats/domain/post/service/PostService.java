9235886595dba0afa8755df985f8cf49c98adf6d
Minsun <minisundev@gmail.com>
2024-01-09 00:44:59 +0900

    feat: Post create 기능 #53

--- src/main/java/com/moayo/moayoeats/domain/post/service/PostService.java
+++ src/main/java/com/moayo/moayoeats/domain/post/service/PostService.java
@@ -1,5 +1,13 @@
 package com.moayo.moayoeats.domain.post.service;
 
+import com.moayo.moayoeats.domain.post.dto.request.PostRequest;
+
 public interface PostService {
+
+    /**
+     *
+     * @param postReq : 글 작성에 필요한 dto
+     */
+    public void createInvitation(PostRequest postReq);
 
 }
--- src/main/java/com/moayo/moayoeats/domain/post/service/impl/PostServiceImpl.java
+++ src/main/java/com/moayo/moayoeats/domain/post/service/impl/PostServiceImpl.java
@@ -1,7 +1,10 @@
 package com.moayo.moayoeats.domain.post.service.impl;
 
+import com.moayo.moayoeats.domain.post.dto.request.PostRequest;
+import com.moayo.moayoeats.domain.post.entity.Post;
 import com.moayo.moayoeats.domain.post.repository.PostRepository;
 import com.moayo.moayoeats.domain.post.service.PostService;
+import java.time.LocalDateTime;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -10,5 +13,22 @@ import org.springframework.stereotype.Service;
 public class PostServiceImpl implements PostService {
 
     private final PostRepository postRepository;
+
+    public void createInvitation(PostRequest postReq){
+        //set deadline to mins after
+        LocalDateTime now = LocalDateTime.now();
+        LocalDateTime deadline = now.plusMinutes(postReq.deadline().getMins());
+
+        Post post = Post.builder()
+            .address(postReq.address())
+            .store(postReq.store())
+            .deliveryCost(postReq.deliveryCost())
+            .minPrice(postReq.minPrice())
+            .deadline(deadline)
+            .category(postReq.category())
+            .build();
+
+        postRepository.save(post);
+    }
 
 }
