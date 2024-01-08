aee492798322cc24636d795161607aae097e09e2
minahYu <mina010311@naver.com>
2024-01-09 01:12:54 +0900

    rename : 디렉토리 구조 수정

--- src/main/java/com/moayo/moayoeats/MoayoeatsApplication.java
+++ src/main/java/com/moayo/moayoeats/MoayoeatsApplication.java
@@ -1,4 +1,4 @@
-package com.example.moayoeats;
+package com.moayo.moayoeats;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
