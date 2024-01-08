01dc861e2fed7ccc115aa8b787a9cd7f14a4471b
minahYu <mina010311@naver.com>
2024-01-08 17:59:02 +0900

    feat : 사용자 controller 생성 #46

---
+++ src/main/java/com/moayo/moayoeats/domain/user/controller/UserController.java
@@ -0,0 +1,17 @@
+package com.moayo.moayoeats.domain.user.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/v1/users")
+public class UserController {
+
+    @PostMapping("/sign-up")
+    public void signup() {
+
+    }
+}
