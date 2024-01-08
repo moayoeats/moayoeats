ed51d74701a6d623ec9672eebd1cc78ef7a0ed9a
minahYu <mina010311@naver.com>
2024-01-08 10:52:12 +0900

    fix : 글로벌 예외처리 수정

--- src/main/java/com/example/moayoeats/global/dto/ApiResponse.java
+++ src/main/java/com/example/moayoeats/global/dto/ApiResponse.java
@@ -2,12 +2,29 @@ package com.example.moayoeats.global.dto;
 
 import lombok.Builder;
 
-@Builder
-public record ApiResponse<T>(
-    int status,
-    String message,
-    T data
-) {
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatusCode;
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ApiResponse<T> {
+    private int status;
+    private String message;
+    private T data;
 
+    public ApiResponse(int status, String message, T data){
+        this.status = status;
+        this.message = message;
+        this.data = data;
+    }
+    public ApiResponse(int status, String message){
+        this.status = status;
+        this.message = message;
+        this.data = null;
+    }
+    public ApiResponse(T data){
+        this.data = data;
+    }
 }
 
