e40d7d037670850209a29c276e5df8e6a9dc370a
Minsun <minisundev@gmail.com>
2024-01-08 16:59:13 +0900

    feat: Menu entity 생성

---
+++ src/main/java/com/moayo/moayoeats/domain/menu/entity/Menu.java
@@ -0,0 +1,41 @@
+package com.moayo.moayoeats.domain.menu.entity;
+
+import com.moayo.moayoeats.domain.post.entity.Post;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "TB_MENU")
+public class Menu {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JoinColumn(name = "post_id", nullable = false)
+    private Post post;
+
+    @Column
+    private String menuname;
+
+    @Column
+    private Long price;
+
+}
