da5ff70e59bbe30cb71809433b85e82610786505
kimhanshin4 <funold1004@gmail.com>
2024-01-08 22:06:43 +0900

    refactor: Security 단 수정 #40 - username => email 변경

--- src/main/java/com/moayo/moayoeats/global/jwt/JwtUtil.java
+++ src/main/java/com/moayo/moayoeats/global/jwt/JwtUtil.java
@@ -38,13 +38,13 @@ public class JwtUtil {
     }
 
     // 토큰 생성
-    public String createToken(String username) {
+    public String createToken(String email) {
         Date date = new Date();
 
         return BEARER_PREFIX +
             Jwts.builder()
-                .setSubject(username) // 사용자 식별자값(ID)
-                .claim(AUTHORIZATION_KEY, username) // 사용자 권한
+                .setSubject(email) // 사용자 식별자값(ID)
+                .claim(AUTHORIZATION_KEY, email) // 사용자 권한
                 .setExpiration(new Date(date.getTime() + TOKEN_TIME)) // 만료 시간
                 .setIssuedAt(date) // 발급일
                 .signWith(key, signatureAlgorithm) // 암호화 알고리즘
