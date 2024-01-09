38dd49d7cf3ca4ba4a5dce0952e2cacc1427670b
minahYu <mina010311@naver.com>
2024-01-09 16:23:26 +0900

    fix: 사용자 예외처리 부분 globalException으로 수정

--- src/main/java/com/moayo/moayoeats/domain/user/exception/UserErrorCode.java
+++ src/main/java/com/moayo/moayoeats/domain/user/exception/UserErrorCode.java
@@ -10,12 +10,12 @@ import org.springframework.http.HttpStatus;
 public enum UserErrorCode implements ErrorCode {
 
     // 400
-    ALREADY_EXIST_USER(HttpStatus.BAD_REQUEST, "이미 존재하는 회원입니다."),
-    NOT_MATCH_PASSWORD(HttpStatus.BAD_REQUEST, "비밀번호가 일치하지 않습니다."),
+    ALREADY_EXIST_USER(HttpStatus.BAD_REQUEST.value(), "이미 존재하는 회원입니다."),
+    NOT_MATCH_PASSWORD(HttpStatus.BAD_REQUEST.value(), "비밀번호가 일치하지 않습니다."),
 
     // 404
-    NOT_EXIST_USER(HttpStatus.NOT_FOUND, "존재하지 않는 사용자입니다.");
+    NOT_EXIST_USER(HttpStatus.NOT_FOUND.value(), "존재하지 않는 사용자입니다.");
 
-    private final HttpStatus httpStatus;
+    private final int httpStatus;
     private final String message;
 }
