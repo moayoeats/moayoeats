b643a04f85d17e293f9850d433e14534b7018722
Minsun <minisundev@gmail.com>
2024-01-09 14:57:48 +0900

    rename: MenuRequest로 이름 변경

--- src/main/java/com/moayo/moayoeats/domain/menu/dto/request/MenuRequest.java
+++ src/main/java/com/moayo/moayoeats/domain/menu/dto/request/MenuRequest.java
@@ -3,7 +3,7 @@ package com.moayo.moayoeats.domain.menu.dto.request;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
-public record MenuCreateRequest(
+public record MenuRequest(
     @NotNull
     Long postId,
     @NotBlank
