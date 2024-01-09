a7cec2ad58d9184b54fe60af9818eb18da28ae85
Xtraa777 <mazino0588@gmail.com>
2024-01-09 15:57:11 +0900

    feat: 웹소켓을 이용한 채팅 구현 #73

---
+++ src/main/java/com/moayo/moayoeats/domain/chat/WebSocketHandler.java
@@ -0,0 +1,22 @@
+package com.moayo.moayoeats.domain.chat;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+@Slf4j
+@Component
+public class WebSocketHandler extends TextWebSocketHandler {
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        String payload = message.getPayload();
+        log.info("payload : {}", payload);
+
+        TextMessage initialGreeting = new TextMessage("Welcome to MoayoEats chat Server");
+        session.sendMessage(initialGreeting);
+    }
+
+}
