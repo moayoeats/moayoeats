68cfb0a78b3c479f72f98f0209bedd2ea2728d42
Minsun <minisundev@gmail.com>
2024-01-09 22:42:20 +0900

    feat: 전체글 조회 기능 구현 #92

--- src/main/java/com/moayo/moayoeats/domain/post/controller/PostController.java
+++ src/main/java/com/moayo/moayoeats/domain/post/controller/PostController.java
@@ -1,14 +1,18 @@
 package com.moayo.moayoeats.domain.post.controller;
 
 import com.moayo.moayoeats.domain.post.dto.request.PostRequest;
+import com.moayo.moayoeats.domain.post.dto.response.BriefPostResponse;
 import com.moayo.moayoeats.domain.post.service.PostService;
 import com.moayo.moayoeats.global.dto.ApiResponse;
 import com.moayo.moayoeats.global.security.UserDetailsImpl;
 import jakarta.validation.Valid;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.neo4j.Neo4jProperties.Authentication;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,6 +33,14 @@ public class PostController {
     ){
         postService.createPost(postReq,userDetails.getUser());
         return new ApiResponse<>(HttpStatus.CREATED.value(), "글을 생성했습니다.");
+    }
+
+    // 모든 글 조회하기
+    @GetMapping("/posts")
+    public ApiResponse<List<BriefPostResponse>> getPosts(
+        @AuthenticationPrincipal UserDetailsImpl userDetails
+    ){
+        return new ApiResponse<>(HttpStatus.OK.value(), "모든 글 조회에 성공했습니다.",postService.getPosts(userDetails.getUser()));
     }
 
 }
--- src/main/java/com/moayo/moayoeats/domain/post/service/PostService.java
+++ src/main/java/com/moayo/moayoeats/domain/post/service/PostService.java
@@ -1,7 +1,9 @@
 package com.moayo.moayoeats.domain.post.service;
 
 import com.moayo.moayoeats.domain.post.dto.request.PostRequest;
+import com.moayo.moayoeats.domain.post.dto.response.BriefPostResponse;
 import com.moayo.moayoeats.domain.user.entity.User;
+import java.util.List;
 
 public interface PostService {
 
@@ -10,5 +12,12 @@ public interface PostService {
      * @param postReq : 글 작성에 필요한 dto
      */
     public void createPost(PostRequest postReq, User user);
+
+    /**
+     *
+     * @param user : login info to sort posts by current location
+     * @return Lists of brief informations about the post
+     */
+    public List<BriefPostResponse> getPosts(User user);
 
 }
