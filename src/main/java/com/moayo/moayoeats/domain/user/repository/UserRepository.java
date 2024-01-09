23d154046d7a3927e128123d7b9279c93817e550
minahYu <mina010311@naver.com>
2024-01-09 15:54:56 +0900

    feat: 사용자 레포지토리에 findByEmail 추가 #74

--- src/main/java/com/moayo/moayoeats/domain/user/repository/UserRepository.java
+++ src/main/java/com/moayo/moayoeats/domain/user/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.moayo.moayoeats.domain.user.repository;
 
 import com.moayo.moayoeats.domain.user.entity.User;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +9,7 @@ import org.springframework.stereotype.Repository;
 public interface UserRepository extends JpaRepository<User, Long> {
 
     boolean existsByEmail(String email);
+
+    Optional<User> findByEmail(String email);
+
 }
