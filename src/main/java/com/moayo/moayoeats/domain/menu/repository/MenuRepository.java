bb2fa0d8548bfd51442972664e5d92dbdcba7665
Minsun <minisundev@gmail.com>
2024-01-09 22:40:40 +0900

    feat: 관련 Repository JPA Query methods 정의 #91

--- src/main/java/com/moayo/moayoeats/domain/menu/repository/MenuRepository.java
+++ src/main/java/com/moayo/moayoeats/domain/menu/repository/MenuRepository.java
@@ -1,8 +1,13 @@
 package com.moayo.moayoeats.domain.menu.repository;
 
 import com.moayo.moayoeats.domain.menu.entity.Menu;
+import com.moayo.moayoeats.domain.post.entity.Post;
+import com.moayo.moayoeats.domain.user.entity.User;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MenuRepository extends JpaRepository<Menu, Long> {
+
+    List<Menu> findAllByUserAndPost(User user, Post post);
 
 }
--- src/main/java/com/moayo/moayoeats/domain/userpost/repository/UserPostRepository.java
+++ src/main/java/com/moayo/moayoeats/domain/userpost/repository/UserPostRepository.java
@@ -1,9 +1,17 @@
 package com.moayo.moayoeats.domain.userpost.repository;
 
+import com.moayo.moayoeats.domain.post.entity.Post;
 import com.moayo.moayoeats.domain.userpost.entity.UserPost;
 import com.moayo.moayoeats.domain.userpost.entity.UserPostId;
+import com.moayo.moayoeats.domain.userpost.entity.UserPostRole;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserPostRepository extends JpaRepository<UserPost, UserPostId> {
+
+    List<UserPost> findAllByPostAndRoleEquals(Post post, UserPostRole role);
+
+    List<UserPost> findAllByPost(Post post);
 
 }
