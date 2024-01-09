5ed29def1e44168a02d468120c9f805b1f37574d
Minsun <minisundev@gmail.com>
2024-01-09 14:57:29 +0900

    feat: Menu 추가하는 기능 #69

--- src/main/java/com/moayo/moayoeats/domain/menu/controller/MenuController.java
+++ src/main/java/com/moayo/moayoeats/domain/menu/controller/MenuController.java
@@ -1,7 +1,13 @@
 package com.moayo.moayoeats.domain.menu.controller;
 
+import com.moayo.moayoeats.domain.menu.dto.request.MenuRequest;
 import com.moayo.moayoeats.domain.menu.service.MenuService;
+import com.moayo.moayoeats.global.dto.ApiResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +17,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class MenuController {
 
     private final MenuService menuService;
+
+    // 글에 본인이 주문할 Menu 추가하기
+    @PostMapping("/menus")
+    public ApiResponse<Void> createMenu(
+        @Valid @RequestBody MenuRequest menuReq
+    ){
+        menuService.createMenu(menuReq);
+        return new ApiResponse<>(HttpStatus.CREATED.value(), "메뉴를 추가했습니다.");
+    }
 
 }
--- src/main/java/com/moayo/moayoeats/domain/menu/service/MenuService.java
+++ src/main/java/com/moayo/moayoeats/domain/menu/service/MenuService.java
@@ -1,5 +1,13 @@
 package com.moayo.moayoeats.domain.menu.service;
 
+import com.moayo.moayoeats.domain.menu.dto.request.MenuRequest;
+
 public interface MenuService {
+
+    /**
+     * 메뉴를 생성한다
+     * @param menuReq : postId, 메뉴 이름, 메뉴 가격이 들어있는 dto
+     */
+    public void createMenu(MenuRequest menuReq);
 
 }
