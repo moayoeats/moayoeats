809b69d4822e5c51a7e0a7823355910eadd2c6d0
Minsun <minisundev@gmail.com>
2024-01-10 18:29:04 +0900

    feat: UserPostErrorCode에 NOT_FOUND_HOST 추가

--- src/main/java/com/moayo/moayoeats/domain/userpost/exception/UserPostErrorCode.java
+++ src/main/java/com/moayo/moayoeats/domain/userpost/exception/UserPostErrorCode.java
@@ -10,7 +10,8 @@ import org.springframework.http.HttpStatus;
 public enum UserPostErrorCode implements ErrorCode {
 
     // 404
-    NOT_FOUND_USERPOST(HttpStatus.NOT_FOUND.value(), "해당 글과 사용자와의 관계를 찾을 수 없습니다.");
+    NOT_FOUND_USERPOST(HttpStatus.NOT_FOUND.value(), "해당 글과 사용자와의 관계를 찾을 수 없습니다."),
+    NOT_FOUND_HOST(HttpStatus.NOT_FOUND.value(), "작성자를 찾을 수 없습니다.");
 
     private final int httpStatus;
     private final String message;
