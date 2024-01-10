0aacac120dba8d058cb6cbe699f9f547e7829784
minahYu <mina010311@naver.com>
2024-01-10 14:21:47 +0900

    feat: offer 레포지토리 생성 #101

---
+++ src/main/java/com/moayo/moayoeats/domain/offer/repository/OfferRepository.java
@@ -0,0 +1,9 @@
+package com.moayo.moayoeats.domain.offer.repository;
+
+import com.moayo.moayoeats.domain.offer.entity.Offer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OfferRepository extends JpaRepository<Offer, Long> {
+
+
+}
