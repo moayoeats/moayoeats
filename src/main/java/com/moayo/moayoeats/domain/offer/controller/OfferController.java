97419d0631f4e4d127d7cde1071c0b993b96c143
minahYu <mina010311@naver.com>
2024-01-11 20:35:05 +0900

    feat: Offer 컨트롤러에 참가신청 조회 메서드 추가 #113

--- src/main/java/com/moayo/moayoeats/domain/offer/controller/OfferController.java
+++ src/main/java/com/moayo/moayoeats/domain/offer/controller/OfferController.java
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -40,6 +41,14 @@ public class OfferController {
 
         offerService.cancelParticipation(offerReq, userDetails.getUser());
         return new ApiResponse<>(HttpStatus.OK.value(), "참가취소가 완료되었습니다.");
+    }
+
+    @GetMapping()
+    public void viewApplication(
+        @RequestBody OfferRequest offerReq,
+        @AuthenticationPrincipal UserDetailsImpl userDetails
+    ) {
+
     }
 
 }
