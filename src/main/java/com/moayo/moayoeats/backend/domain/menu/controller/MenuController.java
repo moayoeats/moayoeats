e887cbb67ff611894ba876cd86a5b5c9a039f9fe
Minsun <minisundev@gmail.com>
2024-01-12 23:48:25 +0900

    feat: 나의 메뉴 조회 Controller단 구현 #174

--- src/main/java/com/moayo/moayoeats/backend/domain/menu/controller/MenuController.java
+++ src/main/java/com/moayo/moayoeats/backend/domain/menu/controller/MenuController.java
@@ -1,11 +1,14 @@
 package com.moayo.moayoeats.backend.domain.menu.controller;
 
 import com.moayo.moayoeats.backend.domain.menu.dto.request.MenuDeleteRequest;
+import com.moayo.moayoeats.backend.domain.menu.dto.request.MenuReadRequest;
 import com.moayo.moayoeats.backend.domain.menu.dto.request.MenuRequest;
+import com.moayo.moayoeats.backend.domain.menu.dto.response.MenuResponse;
 import com.moayo.moayoeats.backend.domain.menu.service.MenuService;
 import com.moayo.moayoeats.backend.global.dto.ApiResponse;
 import com.moayo.moayoeats.backend.global.security.UserDetailsImpl;
 import jakarta.validation.Valid;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -25,22 +28,26 @@ public class MenuController {
 
     // 글에 본인이 주문할 Menu 추가하기
     @PostMapping("/menus")
-    public ApiResponse<Void> createMenu(
-        @Valid @RequestBody MenuRequest menuReq,
-        @AuthenticationPrincipal UserDetailsImpl userDetails
-    ){
-        menuService.createMenu(menuReq,userDetails.getUser());
+    public ApiResponse<Void> createMenu(@Valid @RequestBody MenuRequest menuReq,
+        @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        menuService.createMenu(menuReq, userDetails.getUser());
         return new ApiResponse<>(HttpStatus.CREATED.value(), "메뉴를 추가했습니다.");
     }
 
     // 글에 본인이 주문할 Menu 삭제하기
     @DeleteMapping("/menus")
-    public ApiResponse<Void> deleteMenu(
-        @Valid @RequestBody MenuDeleteRequest menuDeleteReq,
-        @AuthenticationPrincipal UserDetailsImpl userDetails
-    ){
-        menuService.deleteMenu(menuDeleteReq,userDetails.getUser());
+    public ApiResponse<Void> deleteMenu(@Valid @RequestBody MenuDeleteRequest menuDeleteReq,
+        @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        menuService.deleteMenu(menuDeleteReq, userDetails.getUser());
         return new ApiResponse<>(HttpStatus.OK.value(), "메뉴를 삭제했습니다.");
+    }
+
+    //자신의 메뉴 조회
+    @GetMapping("/menus")
+    ApiResponse<List<MenuResponse>> getMenus(@Valid @RequestBody MenuReadRequest menuReadReq,
+        @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        return new ApiResponse<>(HttpStatus.OK.value(), "나의 메뉴 조회",
+            menuService.getMenus(menuReadReq, userDetails.getUser()));
     }
 
 }
