7022e6433e8dd9bd5604661fc0c049504319d8f7
Minsun <minisundev@gmail.com>
2024-01-12 13:04:05 +0900

    refactor: PostCategoryRequest에 @NotNull annotation 추가

--- src/main/java/com/moayo/moayoeats/domain/post/dto/request/PostCategoryRequest.java
+++ src/main/java/com/moayo/moayoeats/domain/post/dto/request/PostCategoryRequest.java
@@ -2,8 +2,10 @@ package com.moayo.moayoeats.domain.post.dto.request;
 
 import com.moayo.moayoeats.domain.post.exception.validator.Category;
 import com.moayo.moayoeats.domain.post.entity.CategoryEnum;
+import jakarta.validation.constraints.NotNull;
 
 public record PostCategoryRequest (
+    @NotNull
     @Category
     String category
 ){
