64a04c377b19b2a94efa29fccaefad7c0a7ff827
Minsun <minisundev@gmail.com>
2024-01-14 02:32:23 +0900

    feat: 주문 완료 관련 ErrorCode정의 #193

--- src/main/java/com/moayo/moayoeats/backend/domain/post/exception/PostErrorCode.java
+++ src/main/java/com/moayo/moayoeats/backend/domain/post/exception/PostErrorCode.java
@@ -12,6 +12,8 @@ public enum PostErrorCode implements ErrorCode {
     //400
     POST_ALREADY_CLOSED(HttpStatus.BAD_REQUEST.value(), "이미 모집이 마감된 게시글 입니다."),
     MENU_NOT_ALLOWED(HttpStatus.BAD_REQUEST.value(), "모집마감 후에는 메뉴를 생성/삭제할 수 없습니다."),
+    CLOSE_FIRST(HttpStatus.BAD_REQUEST.value(), "모집마감이 먼저 완료되어야 합니다."),
+    POST_ALREADY_COMPLETED_ORDER(HttpStatus.BAD_REQUEST.value(), "이미 주문완료가 된 게시글 입니다."),
 
     // 401
     UNAUTHORIZED_USER_ABOUT_POST(HttpStatus.UNAUTHORIZED.value(), "해당 글에 대한 권한이 없습니다."),
