190fc50d17cbdeb02e8dde4aa626567db6762854
Minsun <minisundev@gmail.com>
2024-01-13 12:19:04 +0900

    refactor: 사용하지 않는 객체 import 삭제

--- src/main/java/com/moayo/moayoeats/frontend/domain/post/PostFrontController.java
+++ src/main/java/com/moayo/moayoeats/frontend/domain/post/PostFrontController.java
@@ -1,6 +1,5 @@
 package com.moayo.moayoeats.frontend.domain.post;
 
-import com.moayo.moayoeats.backend.domain.post.controller.PostController;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,8 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequiredArgsConstructor
 @RequestMapping(value = "/api/v1")
 public class PostFrontController {
-
-    private final PostController postController;
 
     @GetMapping("/test/createpost")
     public String createPostPage(){
