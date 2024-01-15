b61f1480fd9edad11987ad3b314e715f8d15a0f3
Xtraa777 <mazino0588@gmail.com>
2024-01-15 11:20:58 +0900

    feat: 채팅방 service

---
+++ src/main/java/com/moayo/moayoeats/backend/domain/chat/service/impl/ChatRoomServiceImpl.java
@@ -0,0 +1,29 @@
+package com.moayo.moayoeats.backend.domain.chat.service.impl;
+
+
+import com.moayo.moayoeats.backend.domain.chat.entity.ChatRoom;
+import com.moayo.moayoeats.backend.domain.chat.repository.ChatRoomRepository;
+import com.moayo.moayoeats.backend.domain.chat.service.ChatRoomService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class ChatRoomServiceImpl implements ChatRoomService {
+
+    private final ChatRoomRepository chatRoomRepository;
+
+    // 채팅방 생성
+    @Override
+    public void createRoom(Long postId) {
+        ChatRoom newRoom = new ChatRoom(postId);
+        chatRoomRepository.save(newRoom);
+    }
+
+    // 채팅방 삭제
+    @Override
+    public void deleteRoom(Long postId) {
+        chatRoomRepository.deleteById(postId);
+    }
+
+}
