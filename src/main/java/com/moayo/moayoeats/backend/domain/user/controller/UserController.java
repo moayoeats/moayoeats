0d76615b634e2d8e9279c9d6558b7d64e351a210
minahYu <mina010311@naver.com>
2024-01-15 16:27:25 +0900

    feat: User 컨트롤러에 비밀번호 수정 메서드 추가 #230

--- src/main/java/com/moayo/moayoeats/backend/domain/user/controller/UserController.java
+++ src/main/java/com/moayo/moayoeats/backend/domain/user/controller/UserController.java
@@ -2,6 +2,7 @@ package com.moayo.moayoeats.backend.domain.user.controller;
 
 import com.moayo.moayoeats.backend.domain.user.dto.request.InfoUpdateRequest;
 import com.moayo.moayoeats.backend.domain.user.dto.request.LoginRequest;
+import com.moayo.moayoeats.backend.domain.user.dto.request.PasswordUpdateRequest;
 import com.moayo.moayoeats.backend.domain.user.dto.request.SignupRequest;
 import com.moayo.moayoeats.backend.domain.user.service.UserService;
 import com.moayo.moayoeats.backend.global.dto.ApiResponse;
@@ -57,6 +58,14 @@ public class UserController {
 
         userService.updateInfo(infoUpdateReq, userDetails.getUser());
         return new ApiResponse<>(HttpStatus.OK.value(), "회원정보를 수정하였습니다.");
+    }
+
+    @PatchMapping("/password")
+    public void updatePassword(
+        @Valid @RequestBody PasswordUpdateRequest passwordUpdateReq,
+        @AuthenticationPrincipal UserDetailsImpl userDetails
+    ) {
+
     }
 
 }
