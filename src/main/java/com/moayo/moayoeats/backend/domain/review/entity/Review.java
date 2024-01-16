f0e743174e0d125f584e9fce8d88f1f7bc8b9374
Minsun <minisundev@gmail.com>
2024-01-16 20:42:53 +0900

    refactor: Review Entity 리팩토링 #270

--- src/main/java/com/moayo/moayoeats/backend/domain/review/entity/Review.java
+++ src/main/java/com/moayo/moayoeats/backend/domain/review/entity/Review.java
@@ -1,9 +1,10 @@
 package com.moayo.moayoeats.backend.domain.review.entity;
 
 import com.moayo.moayoeats.backend.domain.user.entity.User;
-import com.moayo.moayoeats.backend.global.entity.BaseTime;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -12,6 +13,7 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -19,7 +21,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 @Table(name = "tb_review")
-public class Review extends BaseTime {
+public class Review {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -30,84 +32,21 @@ public class Review extends BaseTime {
     private User user;
 
     @Column
-    private Integer score;
+    @Enumerated(EnumType.STRING)
+    private ReviewEnum content;
 
     @Column
     private Integer count;
 
-    @Column
-    private Integer goodmanner;
-
-    @Column
-    private Integer goodtime;
-
-    @Column
-    private Integer goodcomm;
-
-    @Column
-    private Integer badtime;
-
-    @Column
-    private Integer noshow;
-
-    @Column
-    private Integer nomoney;
-
-    @Column
-    private Integer badcomm;
-
-    @Column
-    private Integer badmanner;
-
-    public Review(User user) {
+    @Builder
+    public Review(User user, ReviewEnum content) {
         this.user = user;
-        this.score = 0;
+        this.content = content;
         this.count = 0;
-        this.goodmanner = 0;
-        this.goodtime = 0;
-        this.goodcomm = 0;
-        this.badtime = 0;
-        this.noshow = 0;
-        this.nomoney = 0;
-        this.badcomm = 0;
-        this.badmanner = 0;
     }
 
-    public void increaseScoreAndCount(ScoreEnum scoreEnum) {
-        this.score += scoreEnum.getScore();
-        this.count += 1;
-    }
-
-    public void increaseGoodmanner() {
-        this.goodmanner++;
-    }
-
-    public void increaseGoodtime() {
-        this.goodtime++;
-    }
-
-    public void increaseGoodcomm() {
-        this.goodcomm++;
-    }
-
-    public void increaseBadtime() {
-        this.badtime++;
-    }
-
-    public void increaseNoshow() {
-        this.noshow++;
-    }
-
-    public void increaseNomoney() {
-        this.nomoney++;
-    }
-
-    public void increaseBadcomm() {
-        this.badcomm++;
-    }
-
-    public void increaseBadmanner() {
-        this.badmanner++;
+    public void increaseCount(){
+        this.count++;
     }
 
 }
