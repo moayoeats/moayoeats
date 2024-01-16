104427f2188df86a07ccca249aa27939042efbcd
Minsun <minisundev@gmail.com>
2024-01-16 11:07:45 +0900

    feat:  리뷰 작성할 Order들 조회 Service단 구현 #244

--- src/main/java/com/moayo/moayoeats/backend/domain/review/service/ReviewService.java
+++ src/main/java/com/moayo/moayoeats/backend/domain/review/service/ReviewService.java
@@ -1,5 +1,16 @@
 package com.moayo.moayoeats.backend.domain.review.service;
 
+import com.moayo.moayoeats.backend.domain.order.dto.response.OrderResponse;
+import com.moayo.moayoeats.backend.domain.user.entity.User;
+import java.util.List;
+
 public interface ReviewService {
+
+    /**
+     *
+     * @param user : Order 조회자
+     * @return : OrderResponse
+     */
+    public List<OrderResponse> getOrders(User user);
 
 }
--- src/main/java/com/moayo/moayoeats/backend/domain/review/service/impl/ReviewServiceImpl.java
+++ src/main/java/com/moayo/moayoeats/backend/domain/review/service/impl/ReviewServiceImpl.java
@@ -1,7 +1,13 @@
 package com.moayo.moayoeats.backend.domain.review.service.impl;
 
+import com.moayo.moayoeats.backend.domain.menu.dto.response.MenuResponse;
+import com.moayo.moayoeats.backend.domain.order.dto.response.OrderResponse;
+import com.moayo.moayoeats.backend.domain.order.entity.Order;
+import com.moayo.moayoeats.backend.domain.order.repository.OrderRepository;
 import com.moayo.moayoeats.backend.domain.review.repository.ReviewRepository;
 import com.moayo.moayoeats.backend.domain.review.service.ReviewService;
+import com.moayo.moayoeats.backend.domain.user.entity.User;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -10,5 +16,17 @@ import org.springframework.stereotype.Service;
 public class ReviewServiceImpl implements ReviewService {
 
     private final ReviewRepository reviewRepository;
+    private final OrderRepository orderRepository;
 
+    @Override
+    public List<OrderResponse> getOrders(User user) {
+
+        List<Order> orders = orderRepository.findAllByUser(user);
+        return orders.stream().map(
+            order -> OrderResponse.builder().id(order.getId()).store(order.getStore()).menus(
+                    order.getMenus().stream()
+                        .map(menu -> new MenuResponse(menu.getMenuname(), menu.getPrice())).toList())
+                .receiverName(order.getReceiver().getNickname()).build()).toList();
+
+    }
 }
