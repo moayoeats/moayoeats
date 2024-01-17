39f6efbf6719e18c636973fed5a1cec42b7801d3
kimhanshin4 <funold1004@gmail.com>
2024-01-17 12:20:41 +0900

    refactor: NotificationRepository 리팩토링 - 조회메서드 수정

--- src/main/java/com/moayo/moayoeats/backend/domain/notification/repository/NotificationRepository.java
+++ src/main/java/com/moayo/moayoeats/backend/domain/notification/repository/NotificationRepository.java
@@ -1,10 +1,11 @@
 package com.moayo.moayoeats.backend.domain.notification.repository;
 
-import com.moayo.moayoeats.backend.domain.notification.entity.Notification;
-import java.util.List;
-import org.springframework.data.jpa.repository.JpaRepository;
+import com.moayo.moayoeats.backend.domain.notification.entity.*;
+import java.util.*;
+import org.springframework.data.jpa.repository.*;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
 
-    List<Notification> findAllByUserId(Long id);
+    List<Notification> findAllByUserIdOrderByCreatedAtDesc(Long userId);
+
 }
