cbf1de592998f0a1a69293699bda028cfa882eb6
kimhanshin4 <funold1004@gmail.com>
2024-01-17 12:19:44 +0900

    refactor: NotificationListener 수정 - 필요 매개변수 2개 => 1개 - *EventListener는 매개변수 최대 한 개 허용*

--- src/main/java/com/moayo/moayoeats/backend/domain/notification/event/NotificationListener.java
+++ src/main/java/com/moayo/moayoeats/backend/domain/notification/event/NotificationListener.java
@@ -1,11 +1,10 @@
 package com.moayo.moayoeats.backend.domain.notification.event;
 
-import com.moayo.moayoeats.backend.domain.notification.entity.Notification;
-import com.moayo.moayoeats.backend.domain.notification.repository.NotificationRepository;
-import com.moayo.moayoeats.backend.domain.user.entity.User;
-import lombok.RequiredArgsConstructor;
-import org.springframework.context.event.EventListener;
-import org.springframework.stereotype.Component;
+import com.moayo.moayoeats.backend.domain.notification.entity.*;
+import com.moayo.moayoeats.backend.domain.notification.repository.*;
+import lombok.*;
+import org.springframework.context.event.*;
+import org.springframework.stereotype.*;
 
 @RequiredArgsConstructor
 @Component
@@ -14,9 +13,9 @@ public class NotificationListener {
     private final NotificationRepository notificationRepository;
 
     @EventListener
-    public void RequestNotify(User user, Event event) {
+    public void RequestNotify(Event event) {
         Notification newNotification = Notification.builder()
-            .user(user)
+            .user(event.user())
             .notificationType(event.notificationType())
             .content(event.notificationType().getWord()).build();
         notificationRepository.save(newNotification);
