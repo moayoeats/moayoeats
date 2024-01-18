18b561704c8f44dee6816e3d02c72c8c84a08eae
minahYu <mina010311@naver.com>
2024-01-19 02:09:14 +0900

    feat: 사용자 프론트 컨트롤러에 다른 유저 프로필 메서드 추가 #334

--- src/main/java/com/moayo/moayoeats/frontend/domain/user/UserFrontController.java
+++ src/main/java/com/moayo/moayoeats/frontend/domain/user/UserFrontController.java
@@ -2,7 +2,9 @@ package com.moayo.moayoeats.frontend.domain.user;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 @RequiredArgsConstructor
@@ -21,5 +23,13 @@ public class UserFrontController {
     @GetMapping("/mypage")
     public String myPage() {
         return "/domain/user/mypage";
+    }
+
+    @GetMapping("/user/{otherUserId}")
+    public String otherUserPage(@PathVariable Long otherUserId, Model model) {
+
+        model.addAttribute("otherUserId", otherUserId);
+
+        return "/domain/user/other-user-page";
     }
 }
