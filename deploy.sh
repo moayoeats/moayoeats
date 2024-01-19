69d098b170099575fbe3b8b2baa8d0c910d22251
kimhanshin4 <funold1004@gmail.com>
2024-01-19 18:56:00 +0900

    chore : 자동배포 관련 수정 - deploy.sh CD확인을 위한 임시 수정(테스트용)

--- deploy.sh
+++ deploy.sh
@@ -1,30 +1,26 @@
 # 애플리케이션 배포 스크립트
 # 환경변수 권한 부여 및 저장
-chmod u+x /home/ubuntu .env
-source /home/ubuntu .env
 
 # 애플리케이션 소스코드 디렉토리로 이동
 REPOSITORY=/home/ubuntu/moayoeats
+
+# shellcheck disable=SC2164
 cd $REPOSITORY
 
-# 애플리케이션 이름 정의 #1
-APP_NAME=moayoeats
-JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep '.jar' | tail -n 1) # 가장 최신의 JAR 파일 찾기
-JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
+# shellcheck disable=SC2046
+kill -9 `ps -ef|grep java|awk '{print $2}'`
 
-# 현재 실행 중인 프로세스 확인
-CURRENT_PID=$(pgrep -f $APP_NAME)
+JAR_NAME=$(ls -tr build/libs/*.jar | tail -n 1)
 
-# 현재 실행 중인 애플리케이션이 없으면 종료하지 않음 #2
-if [ -z $CURRENT_PID ]
-then
-  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
-else
-  echo "> kill -15 $CURRENT_PID"
-  sudo kill -15 $CURRENT_PID
-  sleep 5
-fi
+echo "> JAR NAME: $JAR_NAME"
 
-# JAR 파일 배포 #3
-echo "> $JAR_PATH 배포"
-nohup java -jar /home/ubuntu/moayoeats-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
+echo "> $JAR_NAME 에 실행권한 추가"
+
+chmod +x $JAR_NAME
+
+echo "> $JAR_NAME 실행"
+
+chmod u+x /home/ubuntu .env
+source /home/ubuntu .env
+
+nohup java -jar $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
