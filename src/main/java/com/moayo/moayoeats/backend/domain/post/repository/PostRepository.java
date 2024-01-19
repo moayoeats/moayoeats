a085ba958f7d152833291bf5cbedb4d8229a8354
Minsun <minisundev@gmail.com>
2024-01-19 22:16:10 +0900

    refactor: PostRepository의 findAllByCategory parameter CategoryEnum으로 수정 #371

--- src/main/java/com/moayo/moayoeats/backend/domain/post/repository/PostRepository.java
+++ src/main/java/com/moayo/moayoeats/backend/domain/post/repository/PostRepository.java
@@ -1,5 +1,6 @@
 package com.moayo.moayoeats.backend.domain.post.repository;
 
+import com.moayo.moayoeats.backend.domain.post.entity.CategoryEnum;
 import com.moayo.moayoeats.backend.domain.post.entity.Post;
 import java.util.List;
 import java.util.Optional;
@@ -9,7 +10,7 @@ public interface PostRepository extends JpaRepository<Post, Long> {
 
     Optional<Post> findById(Long postId);
 
-    Optional<List<Post>> findAllByCategoryEquals(String category);
+    Optional<List<Post>> findAllByCategoryEquals(CategoryEnum category);
 
     List<Post> findAll();
 
