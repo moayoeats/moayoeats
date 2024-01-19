d57aa062fa93490ae47551cb2c94df274a83f6c0
kimhanshin4 <funold1004@gmail.com>
2024-01-19 20:27:49 +0900

    chore : 자동배포 관련 수정

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,9 @@
 # 애플리케이션 배포 스크립트
-# 환경변수 권한 부여 및 저장
+
 
 # 애플리케이션 소스코드 디렉토리로 이동
 REPOSITORY=/home/ubuntu/moayoeats
+JAR_PATH=/home/ubuntu
 
 # shellcheck disable=SC2164
 cd $REPOSITORY
@@ -10,7 +11,7 @@ cd $REPOSITORY
 # shellcheck disable=SC2046
 kill -9 `ps -ef|grep java|awk '{print $2}'`
 
-JAR_NAME=$(ls -tr build/libs/*.jar | tail -n 1)
+JAR_NAME=$(ls -tr $JAR_PATH/*.jar | tail -n 1)
 
 echo "> JAR NAME: $JAR_NAME"
 
@@ -20,6 +21,7 @@ chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
 
+# 환경변수 권한 부여 및 저장
 chmod u+x /home/ubuntu/moayoeats/env.env
 source /home/ubuntu/moayoeats/env.env
 
