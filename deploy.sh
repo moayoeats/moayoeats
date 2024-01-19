93b824442ee3c0d3d837fec5d99613fb411bc0fa
kimhanshin4 <funold1004@gmail.com>
2024-01-19 23:19:20 +0900

    chore : 자동배포 관련 수정

--- deploy.sh
+++ deploy.sh
@@ -1,33 +1,27 @@
 # 애플리케이션 배포 스크립트
 
-NOHUP_OUT_FILE=$REPOSITORY/nohup.out
-
 # 애플리케이션 소스코드 디렉토리로 이동
 REPOSITORY=/home/ubuntu/moayoeats
 
 # shellcheck disable=SC2164
-cd $REPOSITORY
+cd "$REPOSITORY" || exit
 
-# shellcheck disable=SC2046
-kill -9 `ps -ef|grep java|awk '{print $2}'`
+# 현재 실행 중인 Java 프로세스 종료
+pkill -f "java -jar"
 
-JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
+# 가장 최신 JAR 파일 선택
+JAR_NAME=$(ls -t "$REPOSITORY"/*.jar | head -n 1)
 
-ls -ld $REPOSITORY
-ls -ld $JAR_NAME
-ls -ld /home/ubuntu/moayoeats
-
-echo "> JAR NAME: $JAR_NAME"
-chown ubuntu:ubuntu $JAR_NAME
-
-echo "> $JAR_NAME 에 실행권한 추가"
-
-chmod +rx $JAR_NAME
-
-echo "> $JAR_NAME 실행"
+# 파일 권한 설정
+chmod +x "$JAR_NAME"
+chown ubuntu:ubuntu "$JAR_NAME"
 
 # 환경변수 권한 부여 및 저장
-chmod u+x /home/ubuntu/moayoeats/env.env
-source /home/ubuntu/moayoeats/env.env
+chmod u+x "$REPOSITORY/env.env"
+source "$REPOSITORY/env.env"
 
-nohup java -jar $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+# nohup.out 파일 권한 설정
+chmod +w "$REPOSITORY/nohup.out"
+
+# JAR 파일 실행
+nohup java -jar "$JAR_NAME" > "$REPOSITORY/nohup.out" 2>&1 &
