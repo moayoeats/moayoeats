344edf0b24bbea1a50f47a0bf16fd3d24015f1a7
kimhanshin4 <funold1004@gmail.com>
2024-01-21 00:20:15 +0900

    chore : dev로 PR이 merge될 시 자동 배포(테스트)

--- deploy.sh
+++ deploy.sh
@@ -12,16 +12,9 @@ pkill -f "java -jar"
 # 가장 최신 JAR 파일 선택
 JAR_NAME=$(ls -t "$REPOSITORY"/*.jar | head -n 1)
 
-# 파일 권한 설정
-chmod +x "$JAR_NAME"
-chown ubuntu:ubuntu "$JAR_NAME"
-
 # 환경변수 권한 부여 및 저장
 chmod u+x "$REPOSITORY/env.env"
 source "$REPOSITORY/env.env"
-
-# nohup.out 파일 권한 설정
-chmod +w "$REPOSITORY/nohup.out"
 
 # JAR 파일 실행
 nohup java -jar "$JAR_NAME" > "$REPOSITORY/nohup.out" 2>&1 &
