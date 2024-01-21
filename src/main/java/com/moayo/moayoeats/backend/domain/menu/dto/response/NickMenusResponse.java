e2e3173aa861652da879ec9ae9571aa44b0640c8
minahYu <mina010311@naver.com>
2024-01-22 02:20:10 +0900

    chore: NickMenusResponse에 Builder import 삭제

--- src/main/java/com/moayo/moayoeats/backend/domain/menu/dto/response/NickMenusResponse.java
+++ src/main/java/com/moayo/moayoeats/backend/domain/menu/dto/response/NickMenusResponse.java
@@ -1,9 +1,7 @@
 package com.moayo.moayoeats.backend.domain.menu.dto.response;
 
 import java.util.List;
-import lombok.Builder;
 
-@Builder
 public record NickMenusResponse(
     String nickname,
     List<MenuResponse> menus
