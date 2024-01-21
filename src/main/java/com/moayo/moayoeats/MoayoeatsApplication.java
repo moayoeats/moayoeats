bd6f07a0f8b0299a5006ee074f85cf224d09f0a3
kimhanshin4 <funold1004@gmail.com>
2024-01-22 05:20:36 +0900

    fix: cd test

--- src/main/java/com/moayo/moayoeats/MoayoeatsApplication.java
+++ src/main/java/com/moayo/moayoeats/MoayoeatsApplication.java
@@ -14,6 +14,8 @@ public class MoayoeatsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MoayoeatsApplication.class, args);
+        String classpath = System.getProperty("java.class.path");
+        System.out.println("Classpath: " + classpath);
     }
 
 }
