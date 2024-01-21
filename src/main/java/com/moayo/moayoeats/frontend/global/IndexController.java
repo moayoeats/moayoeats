a2efb5f01b21b482a04cb386220cbe058baad106
kimhanshin4 <funold1004@gmail.com>
2024-01-22 05:04:53 +0900

    fix: cd test

--- src/main/java/com/moayo/moayoeats/frontend/global/IndexController.java
+++ src/main/java/com/moayo/moayoeats/frontend/global/IndexController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @RequiredArgsConstructor
 public class IndexController {
 
-    @GetMapping("index")
+    @GetMapping("/")
     public String indexPage() {
         return "index";
     }
