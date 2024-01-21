59d11dcba6ccf41acd2c2c067cce824b55d47c64
minahYu <mina010311@naver.com>
2024-01-22 02:21:34 +0900

    fix: Offer 응답 dto 변경

--- src/main/java/com/moayo/moayoeats/backend/domain/offer/dto/response/OfferResponse.java
+++ src/main/java/com/moayo/moayoeats/backend/domain/offer/dto/response/OfferResponse.java
@@ -1,12 +1,14 @@
 package com.moayo.moayoeats.backend.domain.offer.dto.response;
 
+import com.moayo.moayoeats.backend.domain.menu.dto.response.MenuResponse;
+import java.util.List;
 import lombok.Builder;
 
 @Builder
 public record OfferResponse(
-    Long postId,
-    Long userId,
-    String userNickname
+    Long offerId,
+    String nickname,
+    List<MenuResponse> menus
 ) {
 
 }
