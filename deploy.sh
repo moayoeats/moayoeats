0418ac9bdac8376f6c28a82e27cab429a9f582fe
kimhanshin4 <funold1004@gmail.com>
2024-01-21 14:53:46 +0900

    chore : CD 테스트 확인

--- deploy.sh
+++ deploy.sh
@@ -1,23 +1,26 @@
 # 애플리케이션 배포 스크립트
+# 환경변수 권한 부여 및 저장
 
 # 애플리케이션 소스코드 디렉토리로 이동
 REPOSITORY=/home/ubuntu/moayoeats
 
 # shellcheck disable=SC2164
-cd "$REPOSITORY" || exit
+cd $REPOSITORY
 
-# 압축 해제
-unzip -qq -o "$GITHUB_SHA.zip" -d "$REPOSITORY"
+# shellcheck disable=SC2046
+kill -9 `ps -ef|grep java|awk '{print $2}'`
 
-# 현재 실행 중인 Java 프로세스 종료
-pkill -f "java -jar"
+JAR_NAME=$(ls -tr build/libs/*.jar | tail -n 1)
 
-# 가장 최신 JAR 파일 선택
-JAR_NAME=$(ls -t "$REPOSITORY"/*.jar | head -n 1)
+echo "> JAR NAME: $JAR_NAME"
 
-# 환경변수 권한 부여 및 저장
-chmod u+x "$REPOSITORY/env.env"
-source "$REPOSITORY/env.env"
+echo "> $JAR_NAME 에 실행권한 추가"
 
-# JAR 파일 실행
-nohup java -jar "$JAR_NAME" > "$REPOSITORY/nohup.out" 2>&1 &
+chmod +x $JAR_NAME
+
+echo "> $JAR_NAME 실행"
+
+chmod u+x /home/ubuntu/moayoeats env.env
+source /home/ubuntu/moayoeats env.env
+
+nohup java -jar $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
