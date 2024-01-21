e2bc4e82ddbdc845b633e7ea79302a00e74dbc2b
kimhanshin4 <funold1004@gmail.com>
2024-01-21 18:34:25 +0900

    refactor: 알림 조회시 내용관련 수정 - 알림 조회시 유저정보 노출 안되게 수정

--- src/main/java/com/moayo/moayoeats/backend/domain/notification/dto/response/NotificationsResponse.java
+++ src/main/java/com/moayo/moayoeats/backend/domain/notification/dto/response/NotificationsResponse.java
@@ -2,13 +2,11 @@ package com.moayo.moayoeats.backend.domain.notification.dto.response;
 
 import com.moayo.moayoeats.backend.domain.notification.entity.Notification;
 import com.moayo.moayoeats.backend.domain.notification.entity.NotificationType;
-import com.moayo.moayoeats.backend.domain.user.entity.User;
 import java.time.LocalDateTime;
 import lombok.Builder;
 
 @Builder
 public record NotificationsResponse(
-    User user,
     NotificationType type,
     String fieldContent,
     LocalDateTime createdAt
@@ -16,7 +14,6 @@ public record NotificationsResponse(
 
     public static NotificationsResponse formWith(Notification notification) {
         return NotificationsResponse.builder()
-            .user(notification.getUser())
             .type(notification.getFieldName())
             .fieldContent(notification.getContent())
             .createdAt(notification.getCreatedAt())
