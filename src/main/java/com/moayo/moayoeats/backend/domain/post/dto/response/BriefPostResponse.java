32d50a6758e4cb31d7b98bda72533d311bf09acc
Minsun <minisundev@gmail.com>
2024-01-25 00:23:27 +0900

    refactor: BriefPostResponse에 PostStatusEnum 필드 추가 #473

--- src/main/java/com/moayo/moayoeats/backend/domain/post/dto/response/BriefPostResponse.java
+++ src/main/java/com/moayo/moayoeats/backend/domain/post/dto/response/BriefPostResponse.java
@@ -1,14 +1,19 @@
 package com.moayo.moayoeats.backend.domain.post.dto.response;
 
+import com.moayo.moayoeats.backend.domain.post.entity.PostStatusEnum;
 import java.time.LocalDateTime;
+import lombok.Builder;
 
+@Builder
 public record BriefPostResponse(
     Long id,
     String author,
     String store,
     Integer minPrice,
     Integer sumPrice,
-    LocalDateTime deadline
+    LocalDateTime deadline,
+    PostStatusEnum status
+
 ) {
 
 }
