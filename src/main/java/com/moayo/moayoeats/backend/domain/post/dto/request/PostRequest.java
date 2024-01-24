a6bacfead9511293e726a4f93a3680b41b3ab357
Minsun <minisundev@gmail.com>
2024-01-24 13:37:05 +0900

    refactor: 글 작성하기 dto 필드 annotation 수정

--- src/main/java/com/moayo/moayoeats/backend/domain/post/dto/request/PostRequest.java
+++ src/main/java/com/moayo/moayoeats/backend/domain/post/dto/request/PostRequest.java
@@ -7,13 +7,21 @@ import jakarta.validation.constraints.NotNull;
 
 public record PostRequest(
 
-    @NotBlank String address,
-    @NotBlank String store,
-    @NotNull Integer minPrice,
-    @NotNull Integer deliveryCost,
+    @NotBlank(message = "주소를 지정해 주세요")
+    String address,
+    @NotBlank(message = "가게명을 입력해 주세요")
+    String store,
+    @NotNull(message = "최소주문금액을 입력해 주세요")
+    Integer minPrice,
+    @NotNull(message = "배달비를 입력해 주세요")
+    Integer deliveryCost,
 
-    @NotNull @Max(59) Integer deadlineMins,
-    @NotNull @Max(3) Integer deadlineHours,
+    @NotNull(message = "마감시간 시를 입력해 주세요")
+    @Max(value = 59,message = "분은 59보다 짧아야 합니다")
+    Integer deadlineMins,
+    @NotNull(message = "마감시간 분을 입력해 주세요")
+    @Max(value = 12, message = "시간은 12시간보다 짧아야 합니다")
+    Integer deadlineHours,
     CategoryEnum category
 
 ) {
