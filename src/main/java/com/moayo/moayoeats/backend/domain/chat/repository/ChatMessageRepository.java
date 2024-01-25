755184bd896207c79a107b84ec35b0938a96fb31
Xtraa777 <mazino0588@gmail.com>
2024-01-25 23:54:52 +0900

    feat: 이전 채팅 내용 불러오기, 메시지 refactor

--- src/main/java/com/moayo/moayoeats/backend/domain/chat/repository/ChatMessageRepository.java
+++ src/main/java/com/moayo/moayoeats/backend/domain/chat/repository/ChatMessageRepository.java
@@ -1,8 +1,9 @@
 package com.moayo.moayoeats.backend.domain.chat.repository;
 
 import com.moayo.moayoeats.backend.domain.chat.entity.ChatMessage;
+import java.util.List;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface ChatMessageRepository extends MongoRepository<ChatMessage, String> {
-
+    List<ChatMessage> findByPostId(String postId);
 }
--- src/main/resources/static/js/chat.js
+++ src/main/resources/static/js/chat.js
@@ -1,11 +1,11 @@
 var stompClient = null;
 
-document.getElementById('send-button').addEventListener('click', function() {
+document.getElementById('send-button').addEventListener('click', function () {
   sendMessageFromInput();
 });
 
-document.addEventListener('keypress', function(event) {
-  if(event.key === 'Enter') {
+document.addEventListener('keypress', function (event) {
+  if (event.key === 'Enter') {
     sendMessageFromInput();
   }
 });
@@ -19,18 +19,23 @@ function sendMessageFromInput() {
 }
 
 function connect(postId, username) {
-  var socket = new SockJS('/ws');
-  stompClient = Stomp.over(socket);
-  stompClient.connect({}, function (frame) {
-    console.log('Connected: ' + frame);
-    stompClient.subscribe('/sub/chats/room/' + postId, function (message) {
-      showMessage(JSON.parse(message.body).content);
+  fetchAndShowHistory(postId, function () {
+    var socket = new SockJS('/ws');
+    stompClient = Stomp.over(socket);
+    stompClient.connect({}, function (frame) {
+      console.log('Connected: ' + frame);
+      stompClient.subscribe('/sub/chats/room/' + postId, function (message) {
+        var receivedMessage = JSON.parse(message.body);
+        showMessage(receivedMessage.sender, receivedMessage.content);
+      });
+
+      var chatMessage = {
+        sender: username,
+        content: "님이 입장하셨습니다."
+      };
+      stompClient.send("/pub/chats/join/" + postId, {},
+          JSON.stringify(chatMessage));
     });
-    var chatMessage = {
-      sender: username,
-      content: username + "님이 입장하셨습니다."
-    };
-    stompClient.send("/pub/chats/join/" + postId, {}, JSON.stringify(chatMessage));
   });
 }
 
@@ -44,17 +49,38 @@ function disconnect() {
 function sendMessage(message) {
   var chatMessage = {
     sender: username,
-    content:  username + " : " + message
+    content: message
   };
-  stompClient.send("/pub/chats/message/" + postId, {}, JSON.stringify(chatMessage));
+  stompClient.send("/pub/chats/message/" + postId, {},
+      JSON.stringify(chatMessage));
 }
 
-function showMessage(message) {
-  console.log('Received message:', message);  // 디버깅 코드 추가
+function showMessage(username, message) {
   var messageArea = document.getElementById('chat-messages');
   var messageElement = document.createElement('div');
   messageElement.classList.add('chat-message');
-  messageElement.innerText = message;
+
+  if (message === "님이 입장하셨습니다.") {
+    messageElement.innerText = username + message;  // 입장 메시지
+  } else {
+    messageElement.innerText = username + " : " + message;  // 일반 메시지
+  }
+
   messageArea.appendChild(messageElement);
   messageArea.scrollTop = messageArea.scrollHeight;
+}
+
+function fetchAndShowHistory(postId, callback) {
+  $.ajax({
+    url: `/chats/history/${postId}`,
+    type: 'GET',
+    success: function (messages) {
+      messages.forEach(function (message) {
+        showMessage(message.sender, message.content);
+      });
+      if (callback) {
+        callback();
+      }
+    }
+  });
 }
