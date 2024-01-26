e38f97258fe99e7327b930d08b1c3a6732f88710
Xtraa777 <mazino0588@gmail.com>
2024-01-26 22:40:04 +0900

    fix: 시간 제거 테스트

--- src/main/java/com/moayo/moayoeats/backend/domain/chat/controlloer/ChatController.java
+++ src/main/java/com/moayo/moayoeats/backend/domain/chat/controlloer/ChatController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequiredArgsConstructor
 @Controller
 public class ChatController {
-
+// 테스트
     private final ChatMessageServiceImpl chatMessageService;
 
     @MessageMapping("/chats/join/{postId}")
