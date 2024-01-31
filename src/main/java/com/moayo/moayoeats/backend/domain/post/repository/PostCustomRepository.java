2a1c54035739fb0549155603069b186e75ac2359
Minsun <minisundev@gmail.com>
2024-01-31 16:56:32 +0900

    feat: PostCustomRepository에 유저 주소와 가까운 순으로 정렬하게 QueryMethod 구현 #395

--- src/main/java/com/moayo/moayoeats/backend/domain/post/repository/PostCustomRepository.java
+++ src/main/java/com/moayo/moayoeats/backend/domain/post/repository/PostCustomRepository.java
@@ -1,5 +1,6 @@
 package com.moayo.moayoeats.backend.domain.post.repository;
 
+import com.moayo.moayoeats.backend.domain.post.entity.CategoryEnum;
 import com.moayo.moayoeats.backend.domain.post.entity.Post;
 import com.moayo.moayoeats.backend.domain.user.entity.User;
 import java.util.List;
@@ -7,5 +8,6 @@ import java.util.List;
 public interface PostCustomRepository {
 
     List<Post> getPostsByDistance(int page,User user);
+    List<Post> getPostsByDistanceAndCategory(int page, User user, CategoryEnum category);
 
 }
--- src/main/java/com/moayo/moayoeats/backend/domain/post/repository/PostCustomRepositoryImpl.java
+++ src/main/java/com/moayo/moayoeats/backend/domain/post/repository/PostCustomRepositoryImpl.java
@@ -1,7 +1,6 @@
 package com.moayo.moayoeats.backend.domain.post.repository;
 
-import static com.querydsl.core.types.dsl.MathExpressions.power;
-
+import com.moayo.moayoeats.backend.domain.post.entity.CategoryEnum;
 import com.moayo.moayoeats.backend.domain.post.entity.Post;
 import com.moayo.moayoeats.backend.domain.post.entity.QPost;
 import com.moayo.moayoeats.backend.domain.user.entity.User;
@@ -9,8 +8,6 @@ import com.querydsl.core.QueryResults;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 
 @RequiredArgsConstructor
@@ -20,19 +17,42 @@ public class PostCustomRepositoryImpl implements PostCustomRepository {
     private final JPAQueryFactory jpaQueryFactory;
 
     @Override
-    public List<Post> getPostsByDistance(int page, User viewer) {
+    public List<Post> getPostsByDistance(int page, User user) {
 
         QPost post = QPost.post;
-
-        Pageable pageable = PageRequest.of(page, 5);
+        int pagesize = 5;
 
         QueryResults<Post> results = jpaQueryFactory
             .selectFrom(post)
-            .orderBy(((post.latitude.subtract(viewer.getLatitude())).multiply((post.latitude.subtract(viewer.getLatitude())))
-                .add((post.longitude.subtract(viewer.getLongitude())).multiply((post.longitude.subtract(viewer.getLongitude())))))
+            .orderBy(((post.latitude.subtract(user.getLatitude())).multiply(
+                    (post.latitude.subtract(user.getLatitude())))
+                .add((post.longitude.subtract(user.getLongitude())).multiply(
+                    (post.longitude.subtract(user.getLongitude())))))
                 .asc())
-            .offset(pageable.getOffset())
-            .limit(pageable.getPageSize())
+            .offset(page)
+            .limit(pagesize)
+            .fetchResults();
+
+        List<Post> closestPosts = results.getResults();
+        return closestPosts;
+    }
+
+    @Override
+    public List<Post> getPostsByDistanceAndCategory(int page, User user, CategoryEnum category) {
+
+        QPost post = QPost.post;
+        int pagesize = 5;
+
+        QueryResults<Post> results = jpaQueryFactory
+            .selectFrom(post)
+            .where(post.category.eq(category))
+            .orderBy(((post.latitude.subtract(user.getLatitude())).multiply(
+                    (post.latitude.subtract(user.getLatitude())))
+                .add((post.longitude.subtract(user.getLongitude())).multiply(
+                    (post.longitude.subtract(user.getLongitude())))))
+                .asc())
+            .offset(page*pagesize)
+            .limit(pagesize)
             .fetchResults();
 
         List<Post> closestPosts = results.getResults();
