1ea5f42f3bfc0f8c3b4594219d68a00a0a85cf5a
Minsun <minisundev@gmail.com>
2024-02-01 23:32:14 +0900

    feat: PostCustomRepository에 OPEN인 글만 전체 조회하는 메서드 구현 #534

--- src/main/java/com/moayo/moayoeats/backend/domain/post/repository/PostCustomRepository.java
+++ src/main/java/com/moayo/moayoeats/backend/domain/post/repository/PostCustomRepository.java
@@ -2,12 +2,15 @@ package com.moayo.moayoeats.backend.domain.post.repository;
 
 import com.moayo.moayoeats.backend.domain.post.entity.CategoryEnum;
 import com.moayo.moayoeats.backend.domain.post.entity.Post;
+import com.moayo.moayoeats.backend.domain.post.entity.PostStatusEnum;
 import com.moayo.moayoeats.backend.domain.user.entity.User;
 import java.util.List;
 
 public interface PostCustomRepository {
 
     List<Post> getPostsByDistance(int page,User user);
+
+    List<Post> getPostsByStatusOrderByDistance(int page, PostStatusEnum status, User user);
     List<Post> getPostsByDistanceAndCategory(int page, User user, CategoryEnum category);
 
     List<Post> getPostsByDistanceAndKeyword(int page, User user, String keyword);
--- src/main/java/com/moayo/moayoeats/backend/domain/post/repository/PostCustomRepositoryImpl.java
+++ src/main/java/com/moayo/moayoeats/backend/domain/post/repository/PostCustomRepositoryImpl.java
@@ -2,6 +2,7 @@ package com.moayo.moayoeats.backend.domain.post.repository;
 
 import com.moayo.moayoeats.backend.domain.post.entity.CategoryEnum;
 import com.moayo.moayoeats.backend.domain.post.entity.Post;
+import com.moayo.moayoeats.backend.domain.post.entity.PostStatusEnum;
 import com.moayo.moayoeats.backend.domain.post.entity.QPost;
 import com.moayo.moayoeats.backend.domain.user.entity.User;
 import com.querydsl.core.QueryResults;
@@ -15,12 +16,12 @@ import org.springframework.stereotype.Repository;
 public class PostCustomRepositoryImpl implements PostCustomRepository {
 
     private final JPAQueryFactory jpaQueryFactory;
+    private final int pagesize = 5;
 
     @Override
     public List<Post> getPostsByDistance(int page, User user) {
 
         QPost post = QPost.post;
-        int pagesize = 5;
         int offset = page*pagesize;
 
         QueryResults<Post> results = jpaQueryFactory
@@ -39,10 +40,18 @@ public class PostCustomRepositoryImpl implements PostCustomRepository {
     }
 
     @Override
+    public List<Post> getPostsByStatusOrderByDistance(int page, PostStatusEnum status, User user) {
+        if(user.getLongitude()==null||user.getLatitude()==null){
+            return getPostsByStatus(page, status);
+        }else{
+            return getPostsByStatus(page, status, user);
+        }
+    }
+
+    @Override
     public List<Post> getPostsByDistanceAndCategory(int page, User user, CategoryEnum category) {
 
         QPost post = QPost.post;
-        int pagesize = 5;
         int offset = page*pagesize;
 
         QueryResults<Post> results = jpaQueryFactory
@@ -64,7 +73,6 @@ public class PostCustomRepositoryImpl implements PostCustomRepository {
     @Override
     public List<Post> getPostsByDistanceAndKeyword(int page, User user, String keyword) {
         QPost post = QPost.post;
-        int pagesize = 5;
         int offset = page*pagesize;
 
         QueryResults<Post> results = jpaQueryFactory
@@ -82,4 +90,40 @@ public class PostCustomRepositoryImpl implements PostCustomRepository {
         List<Post> closestPosts = results.getResults();
         return closestPosts;
     }
+
+    private List<Post> getPostsByStatus(int page, PostStatusEnum status){
+        QPost post = QPost.post;
+        int offset = page*pagesize;
+
+        QueryResults<Post> results = jpaQueryFactory
+            .selectFrom(post)
+            .where(post.postStatus.eq(status))
+            .orderBy(post.deadline
+                .desc())
+            .offset(offset)
+            .limit(pagesize)
+            .fetchResults();
+
+        return results.getResults();
+    }
+
+    private List<Post> getPostsByStatus(int page, PostStatusEnum status,User user){
+        QPost post = QPost.post;
+        int offset = page*pagesize;
+
+        QueryResults<Post> results = jpaQueryFactory
+            .selectFrom(post)
+            .where(post.postStatus.eq(status))
+            .orderBy(((post.latitude.subtract(user.getLatitude())).multiply(
+                    (post.latitude.subtract(user.getLatitude())))
+                .add((post.longitude.subtract(user.getLongitude())).multiply(
+                    (post.longitude.subtract(user.getLongitude())))))
+                .asc())
+            .offset(offset)
+            .limit(pagesize)
+            .fetchResults();
+
+        return results.getResults();
+    }
+
 }
