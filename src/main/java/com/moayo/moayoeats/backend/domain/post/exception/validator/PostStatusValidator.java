23dbfcb41619cb70872f06ccde0efe9a1f386db8
Minsun <minisundev@gmail.com>
2024-02-01 23:30:23 +0900

    feat: parameter가 PostStatusEnum값인지 검증하는 Validator 구현 #534

---
+++ src/main/java/com/moayo/moayoeats/backend/domain/post/exception/validator/PostStatusValidator.java
@@ -0,0 +1,23 @@
+package com.moayo.moayoeats.backend.domain.post.exception.validator;
+
+
+import com.moayo.moayoeats.backend.domain.post.entity.PostStatusEnum;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class PostStatusValidator implements ConstraintValidator<PostStatus, String> {
+
+    @Override
+    public void initialize(PostStatus status) {
+    }
+
+    @Override
+    public boolean isValid(String status,
+        ConstraintValidatorContext cxt) {
+        return status.equals(PostStatusEnum.OPEN.toString())||
+            status.equals(PostStatusEnum.CLOSED.toString())||
+            status.equals(PostStatusEnum.ORDERED.toString())||
+            status.equals(PostStatusEnum.RECEIVED.toString())
+            ;
+    }
+}
