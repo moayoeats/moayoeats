ceb71aeb049314257e164736e32dbe4589ac093a
minahYu <mina010311@naver.com>
2024-02-01 14:36:52 +0900

    feat: TokenService에 relatedIssuanceOfTokens메서드 추가 #548

--- src/main/java/com/moayo/moayoeats/backend/global/jwt/TokenService.java
+++ src/main/java/com/moayo/moayoeats/backend/global/jwt/TokenService.java
@@ -2,16 +2,69 @@ package com.moayo.moayoeats.backend.global.jwt;
 
 import static com.moayo.moayoeats.backend.global.jwt.JwtUtil.REFRESH_TOKEN_TIME;
 
+import io.jsonwebtoken.Claims;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 @RequiredArgsConstructor
 @Service
 public class TokenService {
 
     private final RedisTemplate<String, String> redisTemplate;
+    private final JwtUtil jwtUtil;
+
+    public String relatedIssuanceOfTokens(
+        HttpServletRequest req,
+        HttpServletResponse res
+    ) throws IOException {
+
+        String accessToken = jwtUtil.getTokenFromRequest(req);
+        String refreshToken = jwtUtil.getRefreshTokenFromRequest(req);
+
+        if (StringUtils.hasText(accessToken) && StringUtils.hasText(refreshToken)) {
+            accessToken = jwtUtil.substringToken(accessToken);
+            refreshToken = jwtUtil.substringToken(refreshToken);
+
+            if (jwtUtil.validateToken(accessToken, res)) { // accessToken 유효
+                Claims info = jwtUtil.getUserInfoFromToken(refreshToken);
+
+                try {
+                    if (info == null) {
+                        info = jwtUtil.getUserInfoFromToken(accessToken);
+                    }
+                    String email = info.getSubject();
+                    if (getRefreshToken(email) == null) { // refreshToken 만료시
+                        String newRefreshToken = jwtUtil.createRefreshToken(
+                            email); // refreshToken 생성
+                        saveRefreshToken(email, newRefreshToken);
+                        jwtUtil.addRefreshJwtToCookie(newRefreshToken, res);
+                    }
+                } catch (Exception e) {
+                    throw new RuntimeException("refresh token 생성 실패", e);
+                }
+            } else { // accessToken 만료
+                if (jwtUtil.validateToken(refreshToken, res)) { // refreshToken 유효
+                    Claims info = jwtUtil.getUserInfoFromToken(refreshToken);
+                    String email = info.getSubject();
+                    String newAccessToken = jwtUtil.createToken(email);
+
+                    if (!newAccessToken.equals("")) {
+                        jwtUtil.addJwtToCookie(newAccessToken, res);
+                        return newAccessToken;
+                    }
+                } /*else { */// 만료
+                res.sendRedirect("/login");
+//                }
+            }
+        }
+        return "";
+    }
 
     // refresh token 저장
     public void saveRefreshToken(String email, String refreshToken) {
