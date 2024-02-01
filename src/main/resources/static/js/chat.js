fc876e5636e74046020d21883242c2e9a4e38840
Xtraa777 <mazino0588@gmail.com>
2024-02-01 10:54:49 +0900

    refactor: 웹소켓 ws -> wss

--- src/main/resources/static/js/chat.js
+++ src/main/resources/static/js/chat.js
@@ -20,7 +20,7 @@ function sendMessageFromInput() {
 
 function connect(postId, username) {
   fetchAndShowHistory(postId, function () {
-    var socket = new SockJS('/ws');
+    var socket = new SockJS('/wss');
     stompClient = Stomp.over(socket);
     stompClient.connect({}, function (frame) {
       console.log('Connected: ' + frame);
