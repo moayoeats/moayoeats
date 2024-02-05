d03aa4443fa09f2b1995cd3ac13b08355668bae5
minahYu <mina010311@naver.com>
2024-02-05 16:19:31 +0900

    feat: SignupRequest에 email 관련 message 추가

--- src/main/java/com/moayo/moayoeats/backend/domain/user/dto/request/SignupRequest.java
+++ src/main/java/com/moayo/moayoeats/backend/domain/user/dto/request/SignupRequest.java
@@ -4,7 +4,7 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Pattern;
 
 public record SignupRequest(
-    @Email
+    @Email(message = "email 올바른 형식의 이메일 주소여야 합니다.")
     String email,
     @Pattern(regexp = "^[a-zA-Z0-9!@#$]{8,15}$",
         message = "비밀번호는 a-z, A-Z, 0-9, !@#$ 중 하나 이상을 포함하고 8-15자이어야 합니다.")
