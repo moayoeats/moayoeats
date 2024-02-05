75963bf2d82e357cd9ae1f139705e603d264509b
Xtraa777 <mazino0588@gmail.com>
2024-02-05 15:32:08 +0900

    feat: 채팅 입장 전 참가자 확인

--- src/main/java/com/moayo/moayoeats/frontend/domain/chat/ChatFrontController.java
+++ src/main/java/com/moayo/moayoeats/frontend/domain/chat/ChatFrontController.java
@@ -1,6 +1,11 @@
 package com.moayo.moayoeats.frontend.domain.chat;
 
 import com.moayo.moayoeats.backend.domain.chat.service.ChatRoomService;
+import com.moayo.moayoeats.backend.domain.user.entity.User;
+import com.moayo.moayoeats.backend.domain.user.exception.UserErrorCode;
+import com.moayo.moayoeats.backend.domain.user.repository.UserRepository;
+import com.moayo.moayoeats.backend.domain.userpost.repository.UserPostRepository;
+import com.moayo.moayoeats.backend.global.exception.GlobalException;
 import com.moayo.moayoeats.backend.global.security.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -14,6 +19,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 public class ChatFrontController {
 
     private final ChatRoomService chatRoomService;
+    private final UserPostRepository userPostRepository;
+    private final UserRepository userRepository;
 
     @GetMapping("/chat/{postId}")
     public String getChatPage(
@@ -21,7 +28,18 @@ public class ChatFrontController {
             Model model,
             @AuthenticationPrincipal UserDetailsImpl userDetails
         ) {
+
         if (!chatRoomService.existsById(postId)) {
+            return "redirect:/";
+        }
+
+        Long userId = userRepository.findByNickname(userDetails.getUsername())
+            .map(User::getId)
+            .orElseThrow(() -> new GlobalException(UserErrorCode.NOT_EXIST_USER));
+
+        boolean userPostExists = userPostRepository.findByUserIdAndPostId(userId, postId).isPresent();
+
+        if (!userPostExists) {
             return "redirect:/";
         }
 
