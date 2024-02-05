d6b914ea0e6234933de6474a769121e79e49fb23
minahYu <mina010311@naver.com>
2024-02-05 16:18:49 +0900

    feat: commonTest 생성

---
+++ src/test/java/com/moayo/moayoeats/test/CommonTest.java
@@ -0,0 +1,9 @@
+package com.moayo.moayoeats.test;
+
+public interface CommonTest {
+
+    String TEST_USER_EMAIL = "test00@email.com";
+    String TEST_USER_PASSWORD = "Password123!";
+    String TEST_USER_CHECK_PASSWORD = "Password123!";
+    String TEST_USER_NICKNAME = "사용자1";
+}
